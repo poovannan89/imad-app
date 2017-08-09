212e5229ad94c7b00a852eb25ee2493bf9587a63
poovannan <poovannancse@gmail.com>
2017-08-10 00:07:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,10 @@
 console.log('Loaded!');
 
 //Change text
-var element=document.getElementById('main-text')
+var element=document.getElementById('main-text');
 element.innerHTML ='new Value'
+
+var img=document.getElementById('img');
+img.onclick = function() {
+  img.style.marginLeft = '100px';  
+};
