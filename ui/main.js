10ec984dcb082c8c72f40407e4b471a3192c3061
poovannan <poovannancse@gmail.com>
2017-08-19 08:37:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,25 +1,9 @@
 var button = document.getElementById('counter');
 button.onclick = function() {
     
-    //Create a request object
-    var request = new XMLHttpRequest();
     
-    //Capture the response and store it in a variable
-    request.onreadystatechange = function () {
-        if (request.readyState == XMLHttpRequest.DONE) {
-            //Take some action
-            if (request.status == 200) {
-                var counter = request.responseText;
-                var span = document.getElementById('count')
-                span.innerHTML = counter.toString();
-            }
-        }
-            
-    };
     
-    //Make the request
-    request.open('GET','http://poovannancse.imad.hasura-app.io/counter', true);
-    request.send(null);
+    
 
 };
 
@@ -28,13 +12,29 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
+    //Create a request object
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if (request.readyState == XMLHttpRequest.DONE) {
+            //Take some action
+            if (request.status == 200) {
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list ='';
+                for(var i=0; i<names.length; i++) {
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;                      
+            }
+        }
+            
+    };
     //Make a request to the server and send the name
     //Capture the list of names and render it as a list
-    var names =['name1','name2','name3','name4'];
-    var list ='';
-    for(var i=0; i<names.length; i++) {
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    //Make the request
+    request.open('GET','http://poovannancse.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(null);
 };
